ee3b7d8fd0dd852a875b945a3611717075793e84
curt_wilhelm@yahoo.com <curt_wilhelm@yahoo.com@b26bfd80-931c-cecd-eb32-ede25e580705>
2011-05-20 13:11:05 +0000

    add livehard

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -5,11 +5,12 @@ mydate=`date '+%Y%m%d%H%M%S'`
 mysqldump --defaults-file=/root/.my.cnf  hpdl_production > ${dir}/hpdl_production.sql.${mydate}
 mysqldump --defaults-file=/root/.my.cnf  mtbcoach_wp > ${dir}/mtbcoach.sql.${mydate}
 mysqldump --defaults-file=/root/.my.cnf  trekstore_wp > ${dir}/trekstore.sql.${mydate}
-#mysqldump --defaults-file=/root/.my.cnf bou0809803490917  > ${dir}/bouldermtb.sql.${mydate}
 mysqldump --defaults-file=/root/.my.cnf rxcard_production > ${dir}/rxcard.sql.${mydate}
 mysqldump --defaults-file=/root/.my.cnf rxsupport_production > ${dir}/rxsupport.sql.${mydate}
 mysqldump --defaults-file=/root/.my.cnf pha0913506120138  > ${dir}/pharma.sql.${mydate}
 mysqldump  --defaults-file=/root/.my.cnf race_production  > ${dir}/race.sql.${mydate}
+mysqldump  --defaults-file=/root/.my.cnf livehard  > ${dir}/livehard.sql.${mydate}
 
 find $dir  -name "*.sql.*"  -mtime +15 -exec rm {} \;
-mysqlcheck -oAc  --defaults-file=/root/.my.cnf
+mysqlcheck --defaults-file=/root/.my.cnf -oAc | grep -V OK
+
