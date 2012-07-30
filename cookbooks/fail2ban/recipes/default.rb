7e07d88b07c14be04ae3eb8ff3f9ee31ff6a3900
Curt Wilhelm <curt_wilhelm@yahoo.com>
2012-07-29 20:00:25 -0600

    add readme

---
+++ cookbooks/fail2ban/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: fail2ban
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+pacakge "fail2ban" do
+  action :install
+end
+
+template "/etc/fail2ban/jail.conf" do
+  source 'jail.conf.erb'
+end
+
+template "/etc/fail2ban/fail2ban.conf" do
+  source 'fail2ban.conf.erb'
+  
+end
+
+
+service "fail2ban" do
+  action [:enable,:start]
+end
---
+++ cookbooks/imagemagik/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: imagemagik
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "ImageMagik" do
+  action :install
+end
