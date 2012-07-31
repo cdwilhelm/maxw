13f7af1c544722bd953c16e556797788bae33421
curt_wilhelm@yahoo.com <curt_wilhelm@yahoo.com@b26bfd80-931c-cecd-eb32-ede25e580705>
2012-07-31 12:54:32 +0000

    add s3backup

--- backup2s3.sh
+++ backup2s3.sh
@@ -3,8 +3,8 @@
 date=`date '+%Y%m%d%H%M%S'`
 backup_dir="/backup"
 dirs="/etc/postfix /etc/httpd /etc/fail2ban /etc/vsftpd /etc/shadow /etc/passwd /etc/group /home/cwilhelm /root /rails/*/shared /mysql-backup"
-
-tar -zcpf ${backup_dir}/maxw-backup-${date}.tgz ${dirs}
+exclude="*.log*"
+tar --exclude=$exclude -zcpf ${backup_dir}/maxw-backup-${date}.tgz ${dirs}
 
 find $backup_dir  -name "*.tgz"  -mtime +10 -exec rm {} \;
-s3sync ${backup_dir}/  maxw-backup:
+s3sync --delete  ${backup_dir}/  maxw-backup:
