d6b3248e255d8b933214771b1e590f966e7b02d2
curt_wilhelm@yahoo.com <curt_wilhelm@yahoo.com@b26bfd80-931c-cecd-eb32-ede25e580705>
2012-08-13 15:33:44 +0000

    add incremental backup

--- backup2s3.sh
+++ backup2s3.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+mmin=""
+btype="full"
+if [ -n "$1" ]; then
+  mmin=" -mmin -$1"
+ btype="incrementatal"
+fi
+
 date=`date '+%Y%m%d%H%M%S'`
 backup_dir="/backup"
-dirs="/etc/postfix /etc/httpd /etc/fail2ban /etc/vsftpd /etc/shadow /etc/passwd /etc/group /home/cwilhelm /root /rails/*/shared /mysql-backup /rails/rxcard /www" 
+dirs="/etc/postfix /etc/httpd /etc/fail2ban /etc/vsftpd /etc/shadow /etc/passwd /etc/group /home/cwilhelm /root /rails/*/shared /mysql-backup /rails/rxcard/public/card_logos /www" 
 exclude="*.log*"
-tar --exclude=$exclude -zcpf ${backup_dir}/maxw-backup-${date}.tgz ${dirs}
+
+find ${dirs} ${mmin} -xtype f -print0 | tar  --null --exclude=$exclude  -zcpf ${backup_dir}/maxw-backup-${btype}-${date}.tgz -T -
+#find ${dirs} ${mmin} -xtype f -print0 > /tmp/backupfiles
+#tar -T /tmp/backupfiles --exclude=$exclude  -zcpf ${backup_dir}/maxw-backup-${btype}-${date}.tgz
 
 find $backup_dir  -name "*.tgz"  -mtime +10 -exec rm {} \;
 s3sync --delete  ${backup_dir}/  maxw-backup:
